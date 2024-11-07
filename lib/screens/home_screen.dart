cf324d64c645af3a13dd7e8de08d8fc75d9cc53d
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-07 17:37:31 +0530

    feat [ui]: Add System Ui Overlay Style and Paddings

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,10 +8,15 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
       ),
+      body: const Padding(
+          padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20)),
     );
   }
 }
