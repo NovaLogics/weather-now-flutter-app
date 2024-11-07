0548b1972ae2a12a4708c9b8b1b76dd22bb92cb7
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-07 17:23:47 +0530

    feat [ui]: Add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_now/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Welcome to the Weather App'),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
