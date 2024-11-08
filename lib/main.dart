77c8dfb2a5f2aea01063a14d394b2bb474690ef6
NovaLogics <rajitha.shavinda@gmail.com>
2024-11-08 16:12:50 +0530

    refactor [service]: Separate API Service logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:geolocator/geolocator.dart';
 
 import 'package:weather_now/bloc/weather/weather_bloc.dart';
+import 'package:weather_now/services/api_service.dart';
 import 'package:weather_now/services/location_service.dart';
+import 'package:weather_now/services/weather_service.dart';
 import 'package:weather_now/ui/screens/home_screen.dart';
 
-void main() => runApp(const WeatherNowApp());
+void main() async {
+  // Ensure Flutter is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await _loadEnvVariables();
+
+  runApp(
+    WeatherNowApp(
+      weatherService: WeatherService(apiKey: ApiService().weatherApiKey),
+    ),
+  );
+}
 
 class WeatherNowApp extends StatelessWidget {
-  const WeatherNowApp({super.key});
+  final WeatherService weatherService;
+
+  const WeatherNowApp({super.key, required this.weatherService});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: BlocProvider<WeatherBloc>(
-        create: (context) => WeatherBloc(),
+        create: (context) =>
+            WeatherBloc(weatherFactory: weatherService.weatherFactory),
         child: const WeatherHomeScreen(),
       ),
     );
@@ -35,18 +53,26 @@ class WeatherHomeScreen extends StatelessWidget {
           return const Scaffold(
             body: Center(child: CircularProgressIndicator()),
           );
-        } 
-        else if (snap.hasData) {
+        } else if (snap.hasData) {
           final position = snap.data as Position;
           context.read<WeatherBloc>().add(FetchWeather(position));
           return const HomeScreen();
-        } 
-        else {
+        } else {
           return Scaffold(
             body: Center(child: Text(snap.error.toString())),
           );
         }
       },
     );
+  }
+}
+
+/// Load environment variables from the .env file.
+/// Throws an exception if there is an error loading the file.
+Future<void> _loadEnvVariables() async {
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    throw Exception('Error loading .env file: $e');
   }
 }
