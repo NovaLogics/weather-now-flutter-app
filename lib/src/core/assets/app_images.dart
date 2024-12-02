21ae64a006d0a6cf3dc89c9625e2ee050a05ebad
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-02 19:55:28 +0530

    refactor [ core ]: Update images

---
+++ lib/src/core/assets/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+  const AppImages._();
+  static const String _baseImagePath = 'assets/images';
+
+  static const String thunderstorm = '$_baseImagePath/thunderstorm.png';
+  static const String drizzle = '$_baseImagePath/drizzle.png';
+  static const String rain = '$_baseImagePath/rain.png';
+  static const String snow = '$_baseImagePath/snow.png';
+  static const String atmosphere = '$_baseImagePath/atmosphere.png';
+  static const String clearSky = '$_baseImagePath/clear_sky.png';
+  static const String clouds = '$_baseImagePath/clouds.png';
+  static const String fewClouds = '$_baseImagePath/few_clouds.png';
+}
