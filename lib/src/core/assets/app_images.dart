5b3f0a0218ae79ba69f7e4aaf2875213bf929f7a
NovaLogics <rajitha.shavinda@gmail.com>
2024-12-02 19:58:30 +0530

    refactor [ core ]: Update images

--- lib/src/core/assets/app_images.dart
+++ lib/src/core/assets/app_images.dart
@@ -2,12 +2,12 @@ class AppImages {
   const AppImages._();
   static const String _baseImagePath = 'assets/images';
 
-  static const String thunderstorm = '$_baseImagePath/thunderstorm.png';
-  static const String drizzle = '$_baseImagePath/drizzle.png';
-  static const String rain = '$_baseImagePath/rain.png';
-  static const String snow = '$_baseImagePath/snow.png';
-  static const String atmosphere = '$_baseImagePath/atmosphere.png';
-  static const String clearSky = '$_baseImagePath/clear_sky.png';
-  static const String clouds = '$_baseImagePath/clouds.png';
-  static const String fewClouds = '$_baseImagePath/few_clouds.png';
+  static const String thunderstormWeather = '$_baseImagePath/thunderstorm.png';
+  static const String drizzleWeather = '$_baseImagePath/drizzle.png';
+  static const String rainWeather = '$_baseImagePath/rain.png';
+  static const String snowWeather = '$_baseImagePath/snow.png';
+  static const String atmosphereWeather = '$_baseImagePath/atmosphere.png';
+  static const String clearSkyWeather = '$_baseImagePath/clear_sky.png';
+  static const String cloudsWeather = '$_baseImagePath/clouds.png';
+  static const String fewCloudsWeather = '$_baseImagePath/few_clouds.png';
 }
